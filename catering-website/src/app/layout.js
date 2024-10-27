6076f25ab690ae4567c50f44fb24d9b136189989
Nithin B <116789923+bnithin06@users.noreply.github.com>
2024-10-27 11:59:27 +0000

    project set up done

---
+++ catering-website/src/app/layout.js
@@ -0,0 +1,32 @@
+import localFont from "next/font/local";
+import "./globals.css";
+import Navbar from "@/components/home/Navbar";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata = {
+  title: "Home Page",
+  description: "Created by Nithin..😍",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <Navbar/>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ catering-website/src/components/home/Navbar.jsx
@@ -0,0 +1,16 @@
+const Navbar = () => {
+    return (
+       <>
+       <div className="flex justify-evenly items-center border-b bg-slate-500 h-16"> 
+            <div>
+                Catering-website
+            </div>
+            <div className="md disabled">
+                Home
+            </div>
+       </div>
+       </>
+    );
+}
+
+export default Navbar;
